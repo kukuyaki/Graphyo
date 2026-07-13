ff456b2bf9f6207393c52f42410411cc0752feea
kgforsure <kgforsure@www.kgforsure.horse>
2026-07-13 11:26:12 +0800

    except python part, ny functoin done

--- test.py
+++ test.py
@@ -1,11 +1,4 @@
-from random import sample
-#this is a python file that generate 2 json file(graph.json and steps.json)
-#run_bfs_and_export() will return and save 2 json file 
+def ynChecktest():
+    return [1,1,0,0]
 
-def random_graph_generator(node_c=20,edge_c=10):
-    nodes = [i+1 for i in range(node_c)]
-    edges = [tuple(sample(nodes,2)) for i in range(edge_c)]
-    return nodes,edges
-
-z = random_graph_generator()
-print(z)
+ynChecktest()
